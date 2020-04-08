66496bd649d5aac487f8a7b7fea5cf5679793a27
Benoît Chauvet <benoit.chauvet@gmail.com>
2020-04-08 14:00:13 +0200

    handle command line arguments

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+#!/usr/bin/env python3
+import json
+
 from enties.source import Sources
 from enties.rule import Rules
 
 
-def main():
-    # TODO : source may be loaded from ENV or option
-    sources = Sources("test/sources.yaml")
-    # TODO : rules should be given as main parameter
-    rules = Rules("test/rules.yaml")
-    print(rules.exec(sources))
+def main(arguments):
+    sources = Sources(args.sources)
+    rules = Rules(args.rules)
+    print(json.dumps(rules.exec(sources)))
 
 
 if __name__ == '__main__':
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description='Extract graph of objects from raw files.')
+    parser.add_argument('rules', help='path to a yaml rules file')
+    parser.add_argument('-s', '--sources', help='path to a yaml sources file', required=True)
+    args = parser.parse_args()
+    main(args)
